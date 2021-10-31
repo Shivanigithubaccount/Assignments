58f768b02a29fef2f4bac8e934e371a021a0e826
Shivani Bhutada <92860895+Shivanigithubaccount@users.noreply.github.com>
2021-10-31 16:31:24 +0530

    Add files via upload

--- Java Assignments/src/AssignmentOne/First.java
+++ Java Assignments/src/AssignmentOne/First.java
@@ -4,6 +4,18 @@ public class First {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int num = 153;
+		int originalNum = num;
+		int total =0;
+		
+		while(num>0)
+		{
+			int no =(num%10);
+			num =num/10;
+			total = total+(no*no*no);
+		}
+		
+		System.out.println(total==originalNum ? originalNum + " is a armstrong number": originalNum + "is not an armstrong number");
 
 	}
 
