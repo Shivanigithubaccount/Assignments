e602e6e33d5b5ea8edc278fad9692fb81ab3e5d1
smanojbh <smanojbh@LIN25007689.corp.capgemini.com>
2021-10-31 16:37:10 +0530

    Second Program Commit

---
+++ Java Assignments/src/AssignmentOne/Second.java
@@ -0,0 +1,30 @@
+package AssignmentOne;
+
+public class Second {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int from =100;
+		int upto= 999;
+		
+		for(int i=from;i<=upto; i++)
+		{
+			int temp =i;
+			int total =0;
+			while (temp > 0)
+			{
+				int sn =(temp % 10);
+				temp = temp /10;
+				total = total+(sn*sn*sn);
+			}
+			
+			if(total==i)
+			{
+				System.out.println(" " + i);
+			}
+			total=0;
+		}
+
+	}
+
+}
