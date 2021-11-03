2a674e572692403644c754613a17e2910c6933d1
smanojbh <smanojbh@LIN25007689.corp.capgemini.com>
2021-11-03 13:30:43 +0530

    First String Program Commit

---
+++ Java Assignments/src/AssignmentString/LengthOfString.java
@@ -0,0 +1,14 @@
+package AssignmentString;
+
+public class LengthOfString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String str= "Hello World";
+		
+		System.out.println(str.length());
+
+	}
+
+}
